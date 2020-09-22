06694e3bcf814631768bf74b9b999c420d9b2310
念着倒才子傻 <1270172401@qq.com>
2020-09-22 20:06:36 +0800

    add nacos

--- gateway/src/main/java/com/small/goal/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/small/goal/gateway/GatewayApplication.java
@@ -1,12 +1,16 @@
 package com.small.goal.gateway;
 
+import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+import com.alibaba.nacos.api.config.annotation.NacosProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 念着倒才子傻
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
